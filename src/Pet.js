17837d8c24fb7f8af5cf2a526f2c7a9f40abbe8c
John Gitahi <gitahi109@gmail.com>
2023-01-04 23:18:58 +0300

    useState and hooks

--- src/Pet.js
+++ src/Pet.js
@@ -1,9 +1,12 @@
+// eslint-disable-next-line no-unused-vars
+import style from "./styles.module.css";
+
 const Pet = (props) => {
   return (
     <div>
-      <h1>{props.name}</h1>
-      <h2>{props.animal}</h2>
-      <h2>{props.breed}</h2>
+      <h1 className="h1">{props.name}</h1>
+      <h2 className="h1">{props.animal}</h2>
+      <h2 className={style.h1}>{props.breed}</h2>
     </div>
   );
 };
---
+++ src/SearchParams.js
@@ -0,0 +1,72 @@
+import { useState } from "react";
+
+const ANIMALS = ["rabbit", "cow", "bird", "cat", "dog"];
+const BREEDS = ["rex", "bichon", "k9"];
+
+const SearchParams = () => {
+  // const location = "Nakuru, KE";
+  const [location, setLocation] = useState("");
+  const [animal, setAnimal] = useState("");
+  const [breed, setBreed] = useState("");
+
+  return (
+    <div className="search-params">
+      <form>
+        <label htmlFor="location">
+          Location {location}
+          <input
+            id="location"
+            value={location}
+            placeholder="Location"
+            onChange={(e) => setLocation(e.target.value)}
+          />
+        </label>
+        <label htmlFor="animal">
+          Animal
+          <select
+            id="animal"
+            value={animal}
+            onChange={(e) => {
+              setAnimal(e.target.value);
+              setBreed("");
+            }}
+            onBlur={(e) => {
+              setAnimal(e.target.value);
+              setBreed("");
+            }}
+          >
+            <option />
+            {ANIMALS.map((animal) => (
+              <option key={animal} value={animal}>
+                {animal}
+              </option>
+            ))}
+          </select>
+        </label>
+        <label htmlFor="breed">
+          Breed
+          <select
+            id="breed"
+            value={animal}
+            onChange={(e) => {
+              setBreed(e.target.value);
+            }}
+            onBlur={(e) => {
+              setBreed(e.target.value);
+            }}
+          >
+            <option />
+            {BREEDS.map((breed) => (
+              <option key={breed} value={breed}>
+                {breed}
+              </option>
+            ))}
+          </select>
+        </label>
+        <button>Submit</button>
+      </form>
+    </div>
+  );
+};
+
+export default SearchParams;
